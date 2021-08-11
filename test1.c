8874b0348dc914cbadd6341dfb87035bafe3f8df
Liien <18007137943@163.com>
2021-08-11 12:18:33 +0800

    20210810练习

---
+++ test1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    //char ch = 'A';
+    //printf("%c\n", ch);
+    //int age = 17;
+    //printf("%d\n", age);
+    //long num = 110;
+    //printf("%d\n", num);
+    //float f = 5.0;
+    //printf("%f\n", f);
+    //double d = 3.14;
+    //printf("%lf\n", d);
+    return 0;
+}
