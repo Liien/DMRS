ed0e21006de8e8d2b97b70ceca30d79edb0b25d4
Liien <18007137943@163.com>
2021-08-11 19:54:54 +0800

    0811第二节课再补充

---
+++ 0811(2).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+//int a = 100;
+//int global = 2021;
+
+//void test()
+//{
+	//printf("test()-- %d\n", global);
+//}
+
+int main()
+{
+	//int a = 10;
+	//printf("%d\n", a);
+	//int num1 = 0;
+	//int num2 = 0;
+	//int sum = 0;
+	//scanf_s("%d%d", &num1, &num2);
+	//sum = num1 + num2;
+	//printf("sum = %d\n", sum);
+	//test();
+	//printf("%d\n", global);
+	//未声明的标志符
+	//声明extern外部符号的
+	//extern int g_val;
+	//printf("g_val = %d\n",  g_val);
+
+	return 0;
+
+}
