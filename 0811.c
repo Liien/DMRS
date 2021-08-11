dde4b211bcc9db76889050b667f814ea00692369
Liien <18007137943@163.com>
2021-08-11 13:06:06 +0800

    0811第二节课补充

---
+++ 0811.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    //char ch = 'A';
+    //printf("%c\n", ch);
+    //int age = 17;
+    //printf("%d\n", age);
+    //long num = 110;
+    //printf("%d\n", num);
+    //float f = 5.0;
+    //printf("%f\n", f);
+    //double d = 3.14;
+    //printf("%lf\n", d);
+    printf("%d\n", sizeof(char));
+    printf("%d\n", sizeof(short));
+    printf("%d\n", sizeof(int));
+    printf("%d\n", sizeof(long));
+    printf("%d\n", sizeof(long long));
+    printf("%d\n", sizeof(float));
+    printf("%d\n", sizeof(double));
+    return 0;
+}
